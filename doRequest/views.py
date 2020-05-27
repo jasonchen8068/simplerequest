cbfd4db051b91027113dc88c40c4a5bf9336e9ff
jasonchen8068 <767316856@qq.com>
2020-05-27 15:19:06 +0800

    登录功能

--- doRequest/views.py
+++ doRequest/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 # Create your views here.
 def test(request):
     return HttpResponse("hello,tester")
+
+def login(request):
+    if request.method == 'GET':
+        return render_to_response('login.html')
+    elif request.method == 'POST':
+        return HttpResponse("hello,tester")
--- simplerequest/urls.py
+++ simplerequest/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from doRequest import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.login),
+    url('api/login', views.login)
 ]
